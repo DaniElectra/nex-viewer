2a189755962f0b0d266600438a14ad460bc4345a
Jonathan Barrow <jonbarrow1998@gmail.com>
2024-06-08 13:49:44 -0400

    chore(types): update type imports for Packet type def

--- src/types/nex/packet.ts
+++ src/types/nex/packet.ts
@@ -1,6 +1,6 @@
-import PRUDPPacketV0 from '@/nex/prudp-packetv0';
-import PRUDPPacketV1 from '@/nex/prudp-packetv1';
-import RawRMCPacket from '@/nex/raw-rmc-packet';
+import type PRUDPPacketV0 from '@/nex/prudp-packetv0';
+import type PRUDPPacketV1 from '@/nex/prudp-packetv1';
+import type RawRMCPacket from '@/nex/raw-rmc-packet';
 
 type Packet = PRUDPPacketV0 | PRUDPPacketV1 | RawRMCPacket;
 
