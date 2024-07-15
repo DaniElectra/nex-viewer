191a1b931bca0a38322e8fead795e8bd9d28eb83
Jonathan Barrow <jonbarrow1998@gmail.com>
2024-07-15 14:27:34 -0400

    feat: add NintendoLoginData/AccountExtraInfo types

---
+++ src/nex/protocols/secure-connection/types/nintendo-login-data.ts
@@ -0,0 +1,35 @@
+import Data from '@/nex/types/data';
+import String from '@/nex/types/string';
+import AnyDataHolder from '@/nex/types/any-data-holder';
+import type NEXByteStream from '@/nex/byte-stream';
+
+export default class NintendoLoginData extends Data {
+	public get typeName(): string {
+		return 'NintendoLoginData';
+	}
+
+	private token = new String();
+
+	public extractFrom(stream: NEXByteStream): void {
+		this.extractHeaderFrom(stream);
+
+		this.token.extractFrom(stream);
+	}
+
+	public new(): this {
+		return new (this.constructor as new () => this)();
+	}
+
+	public toJSON(): Record<string, any> {
+		return {
+			__version: this.structureVersion,
+			__displayTypeName: this.typeName,
+			__typeName: this.typeName,
+			__fields: {
+				token: this.token
+			}
+		};
+	}
+}
+
+AnyDataHolder.Classes['NintendoLoginData'] = NintendoLoginData;
--- src/nex/types/data.ts
+++ src/nex/types/data.ts
@@ -4,14 +4,17 @@ import type NEXByteStream from '@/nex/byte-stream';
 // * Data has no fields itself.
 // * This is the parent class for all types which are allowed in AnyDataHolder
 export default class Data extends Structure {
-	public readonly typeName = 'Data';
+	// * Make this a getter so it can be overridden by child classes
+	public get typeName(): string {
+		return 'Data';
+	}
 
 	public extractFrom(stream: NEXByteStream): void {
 		this.extractHeaderFrom(stream);
 	}
 
-	public new(): Data {
-		return new Data();
+	public new(): this {
+		return new (this.constructor as new () => this)();
 	}
 
 	public toJSON(): Record<string, any> {
