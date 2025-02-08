4f0830e92a43ad7f31d37b52020326b2065d6085
Jonathan Barrow <jonbarrow1998@gmail.com>
2025-02-08 00:37:57 -0500

    chore: removed comment in NEX Title type def

--- src/types/nex/serialized-connection.ts
+++ src/types/nex/serialized-connection.ts
@@ -1,23 +1,7 @@
 import type RMCMessage from '@/nex/rmc-message';
 import type NEXByteStreamSettings from '@/types/nex/byte-stream-settings';
 import type ServiceProtocol from '@/types/nex/service-protocol';
-/*
-export type Title = {
-	name: string;
-	game_server_id: string;
-	access_key: string;
-	library_versions: {
-		main: string;
-		ranking: string;
-		datastore: string;
-		match_making: string;
-		messaging: string;
-		utility: string;
-	};
-	settings: NEXByteStreamSettings;
-	title_ids: string[];
-};
-*/
+
 export interface Title {
 	name: string;
 	gameServerID: string;
