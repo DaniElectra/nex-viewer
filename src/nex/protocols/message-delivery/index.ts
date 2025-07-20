541929dc3e3f65eaa518c02f3749f189d9789f74
Jonathan Barrow <jonbarrow1998@gmail.com>
2025-07-20 08:43:19 -0400

    feat: add MessageDelivery protocol

---
+++ src/nex/protocols/message-delivery/index.ts
@@ -0,0 +1,54 @@
+import RMCMessage from '@/nex/rmc-message';
+import * as Methods from '@/nex/protocols/message-delivery/methods';
+import type Packet from '@/types/nex/packet';
+
+export default class MessageDeliveryProtocol {
+	static ID = 0x1B;
+	static Name = 'MessageDelivery';
+
+	static Methods = {
+		DeliverMessage: 0x1,
+		DeliverMessageMultiTarget: 0x2
+	};
+
+	private static handlers: Record<number, (message: RMCMessage) => any> = {
+		0x01: MessageDeliveryProtocol.DeliverMessage,
+		0x02: MessageDeliveryProtocol.DeliverMessageMultiTarget
+	};
+
+	static handlePacket(packet: Packet): void {
+		if (!packet.message) {
+			// * This will never happen. Only checked to make TypeScript happy
+			return;
+		}
+
+		const methodID = packet.message.methodID;
+		const handler = MessageDeliveryProtocol.handlers[methodID];
+
+		if (!handler) {
+			packet.message.methodName = `UnknownMethod_0x${methodID.toString(16).toUpperCase().padStart(2, '0')}`;
+			return;
+		}
+
+		const messageDecoder = handler(packet.message);
+
+		packet.message.parameters = new messageDecoder(packet.message);
+		packet.message.methodName = messageDecoder.Name;
+	}
+
+	private static DeliverMessage(message: RMCMessage): typeof Methods.DeliverMessage.Request | typeof Methods.DeliverMessage.Response {
+		if (message.type === RMCMessage.REQUEST) {
+			return Methods.DeliverMessage.Request;
+		} else {
+			return Methods.DeliverMessage.Response;
+		}
+	}
+
+	private static DeliverMessageMultiTarget(message: RMCMessage): typeof Methods.DeliverMessageMultiTarget.Request | typeof Methods.DeliverMessageMultiTarget.Response {
+		if (message.type === RMCMessage.REQUEST) {
+			return Methods.DeliverMessageMultiTarget.Request;
+		} else {
+			return Methods.DeliverMessageMultiTarget.Response;
+		}
+	}
+}
---
+++ src/nex/protocols/message-delivery/methods/deliver-message-multi-target.ts
@@ -0,0 +1,39 @@
+import NEXByteStream from '@/nex/byte-stream';
+import List from '@/nex/types/list';
+import PID from '@/nex/types/pid';
+import AnyDataHolder from '@/nex/types/any-data-holder';
+import type RMCMessage from '@/nex/rmc-message';
+
+// TODO - Add strict types for toJSON methods
+
+export class Request {
+	public static Name = 'DeliverMessageMultiTarget';
+
+	private lstTarget = new List(new PID());
+	private oUserMessage = new AnyDataHolder();
+
+	constructor(message: RMCMessage) {
+		const stream = new NEXByteStream(message.parametersData!, message.connection.title);
+
+		this.lstTarget.extractFrom(stream);
+		this.oUserMessage.extractFrom(stream);
+	}
+
+	public toJSON(): any {
+		return {
+			lstTarget: this.lstTarget,
+			oUserMessage: this.oUserMessage
+		};
+	}
+}
+
+// * No response data
+export class Response {
+	public static Name = 'DeliverMessageMultiTarget';
+
+	constructor() {}
+
+	public toJSON(): any {
+		return {};
+	}
+}
