d8355595d46bdb3e5d2f9703c52c0f6ed4a83aac
Daniel López Guimaraes <danielectra@outlook.com>
2026-03-01 16:56:29 +0000

    fix(ticket-granting): Support pre-NEX 3.0 AuthenticationInfo

--- src/nex/protocols/ticket-granting/types/authentication-info.ts
+++ src/nex/protocols/ticket-granting/types/authentication-info.ts
@@ -1,3 +1,4 @@
+import * as semver from 'compare-versions';
 import Data from '@/nex/types/data';
 import RVString from '@/nex/types/string';
 import UInt32 from '@/nex/types/uint32';
@@ -11,8 +12,8 @@ export default class AuthenticationInfo extends Data {
 
 	private m_authToken = new RVString();
 	private m_ngsVersion = new UInt32();
-	private m_authTokenType = new UInt8();
-	private m_serverVersion = new UInt32();
+	private m_authTokenType?: UInt8; // * NEX 3.0
+	private m_serverVersion?: UInt32; // * NEX 3.0
 
 	public extractFrom(stream: NEXByteStream): void {
 		super.extractFrom(stream);
@@ -21,8 +22,14 @@ export default class AuthenticationInfo extends Data {
 
 		this.m_authToken.extractFrom(stream);
 		this.m_ngsVersion.extractFrom(stream);
-		this.m_authTokenType.extractFrom(stream);
-		this.m_serverVersion.extractFrom(stream);
+
+		if (semver.satisfies(stream.title.libraryVersions.main, '>=3.0.0')) {
+			this.m_authTokenType = new UInt8();
+			this.m_authTokenType.extractFrom(stream);
+
+			this.m_serverVersion = new UInt32();
+			this.m_serverVersion.extractFrom(stream);
+		}
 	}
 
 	public new(): this {
@@ -30,17 +37,25 @@ export default class AuthenticationInfo extends Data {
 	}
 
 	public toJSON(): Record<string, any> {
-		return {
+		const json: Record<string, any> = {
 			__parent: super.toJSON(),
 			__version: this.structureVersion,
 			__displayTypeName: this.typeName,
 			__typeName: this.typeName,
 			__fields: {
 				m_authToken: this.m_authToken,
-				m_ngsVersion: this.m_ngsVersion,
-				m_authTokenType: this.m_authTokenType,
-				m_serverVersion: this.m_serverVersion
+				m_ngsVersion: this.m_ngsVersion
 			}
 		};
+
+		if (this.m_authTokenType !== undefined) {
+			json.__fields.m_authTokenType = this.m_authTokenType;
+		}
+
+		if (this.m_serverVersion !== undefined) {
+			json.__fields.m_serverVersion = this.m_serverVersion;
+		}
+
+		return json;
 	}
 }
