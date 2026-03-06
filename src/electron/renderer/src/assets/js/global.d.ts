f4d62f00490629add22607fb5b13465a96125b76
Jonathan Barrow <jonbarrow1998@gmail.com>
2026-03-06 13:24:26 -0500

    chore: replace preload/index.d.ts with global.d.ts
    the preload file wasnt being used anyway and just contained the same types as the global file

--- src/electron/renderer/src/assets/js/global.d.ts
+++ src/electron/renderer/src/assets/js/global.d.ts
@@ -1,12 +1,16 @@
+import type { ElectronAPI } from '@electron-toolkit/preload';
 import type { SerializedMessage } from '@/types/serialized-message';
+import type { SettingsJSON } from '@/types/settings';
 
 declare global {
 	interface Window {
+		electron: ElectronAPI;
 		api: {
 			ready: () => void;
 			onClearSections: (callback: () => void) => void;
 			onSerializedMessage: (callback: (serializedMessage: SerializedMessage) => void) => void;
-			copyToClipboard: (text: string) => void;
+			onSettings: (callback: (newSettings: SettingsJSON) => void) => void;
+			copyToClipboard: (text: string) => void; // TODO - Should this go on the electron global instead?
 		};
 	}
 }
