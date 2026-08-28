f4b71145cf4fe2ae9b97d8dcad5883080af7aca8
Jonathan Barrow <jonbarrow1998@gmail.com>
2026-08-28 16:53:39 -0400

    feat(nex): add pokemon gen7 subscription protocol

---
+++ src/nex/protocols/subscription/pokemon-generation-7/methods/index.ts
@@ -0,0 +1,2 @@
+export * as UnknownMethod0xE from '@/nex/protocols/subscription/pokemon-generation-7/methods/unknown-method-e';
+export * as UnknownMethod0xF from '@/nex/protocols/subscription/pokemon-generation-7/methods/unknown-method-f';
